b33f39d8e8c8172e70759aef5a969113b1043009
Valeria Baranivska <114081412+tobitoria@users.noreply.github.com>
2023-09-30 16:19:58 +0300

    Task 2

--- Lab-1/src/main/java/StringCalculator.java
+++ Lab-1/src/main/java/StringCalculator.java
@@ -9,26 +9,22 @@ public class StringCalculator {
         boolean hasInvalidCharacters = false; // Track if there are invalid characters
 
         for (String num : numberArray) {
-            if (!num.matches("^\\d+$")) {
+            if (!num.matches("^-?\\d+(\\.\\d+)?$")) {
                 hasInvalidCharacters = true;
                 break; // Exit the loop once invalid characters are found
             }
         }
 
         if (hasInvalidCharacters) {
-            System.out.println("Input contains invalid characters.");
+            System.out.println("Input contains invalid characters!");
             return 0; // Return 0 for invalid input
         }
+        int sum = 0;
 
-        if (numberArray.length == 1) {
-            return Integer.parseInt(numberArray[0]);
-        } else if (numberArray.length == 2) {
-            int num1 = Integer.parseInt(numberArray[0]);
-            int num2 = Integer.parseInt(numberArray[1]);
-            return num1 + num2;
-        } else {
-            System.out.println("Too many numbers");
-            return 0; // Return 0 for cases with too many numbers
+        for (String num : numberArray) {
+            sum += Integer.parseInt(num); // Sum all valid numbers
         }
+
+        return sum;
     }
 }
--- Lab-1/src/test/java/StringCalculatorTest.java
+++ Lab-1/src/test/java/StringCalculatorTest.java
@@ -21,4 +21,5 @@ public class StringCalculatorTest {
         assertEquals(3, result);
     }
 
+
 }
