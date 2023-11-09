ffa7b20fb97beb07742c347335f2f3589359faf0
Valeria Baranivska <114081412+ValeriiaBaranivska@users.noreply.github.com>
2023-11-09 17:58:20 +0200

    Next steps

--- Lab-2/src/main/java/org/example/Main.java
+++ Lab-2/src/main/java/org/example/Main.java
@@ -1,13 +1,51 @@
 package org.example;
-
+import java.util.Arrays;
 public class Main {
-    public Main() {
-    }
+        public static void main (String[]args){
+            Matrix matrix1 = new Matrix(3, 3);//mutable matrix
+            double[][] a = new double[][]{
+                    {1.0, 2.0, 3.0},
+                    {4.0, 5.0, 6.0},
+                    {7.0, 8.0, 9.0}
+            };
+            matrix1.fillMatrix(a);
+            System.out.println("Matrix 1:");
+            matrix1.printMatrix();
 
-    public static void main(String[] args) {
-        Matrix matrix = new Matrix(3, 3);
-        double[][] customValues = new double[][]{{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}};
-        matrix.fillMatrix(customValues);
-        matrix.printMatrix();
+//            Matrix copy = new Matrix(matrix1);
+//            System.out.println("Matrix b:");
+//            copy.printMatrix();
+//
+//            System.out.println("Matrix empty:");
+//            Matrix empty = new Matrix(); //empty matrix
+//            empty.printMatrix();
+//
+//            System.out.println("Matrix random:");
+//            Matrix r = new Matrix(3, 3);//random matrix
+//            r.RandomMatrix();
+//            r.printMatrix();
+
+//            //блок getter
+//            double[] column = matrix1.getColumn(2);
+//            System.out.println("Column: " + Arrays.toString(column));
+//
+//             double[] row = matrix1.getRow(1);
+//            System.out.println("Row: " + Arrays.toString(row));
+//
+//            double elem = matrix1.getElement(1, 2);
+//            System.out.println("Element: " + elem);
+
+            //блок setter
+//            matrix1.setColumn(1, new double[]{1.0, 1.0, 1.0});
+//            matrix1.printMatrix();//змінений стовпець
+//
+//            matrix1.setRow(2, new double[]{0.0,-2.0,3.4});
+//            matrix1.printMatrix();//змінений рядок
+//
+//            matrix1.setElement(1,2,-10);
+//            matrix1.printMatrix();//змінений елемент
+
+//            System.out.printf("Rows: %d \nColumns: %d\n", matrix1.getNumRows(), matrix1.getNumColumns());//розмірність матриці
+
+        }
     }
-}
--- Lab-2/src/main/java/org/example/Matrix.java
+++ Lab-2/src/main/java/org/example/Matrix.java
@@ -1,33 +1,32 @@
 package org.example;
+import java.util.Random;
+import java.util.Arrays;
 
 public class Matrix {
     private int rows;
     private int columns;
     private double[][] data;
-
+    // конструктор порожньої матриці
     public Matrix() {
         this.rows = 0;
         this.columns = 0;
         this.data = new double[0][0];
     }
-
+    //матриці заданого розміру
     public Matrix(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
         this.data = new double[this.rows][this.columns];
     }
-
-    public Matrix(double[][] matrixData) {
-        this.rows = matrixData.length;
-        this.columns = matrixData[0].length;
+    //конструктор копії матриці
+    public Matrix(Matrix otherMatrix) {
+        this.rows = otherMatrix.rows;
+        this.columns = otherMatrix.columns;
         this.data = new double[this.rows][this.columns];
-
-        for(int i = 0; i < this.rows; ++i) {
-            for(int j = 0; j < this.columns; ++j) {
-                this.data[i][j] = matrixData[i][j];
-            }
+        // Copy the values from the otherMatrix to the new matrix
+        for (int i = 0; i < this.rows; ++i) {
+            this.data[i] = Arrays.copyOf(otherMatrix.data[i], this.columns);
         }
-
     }
 
     public void fillMatrix(double[][] values) {
@@ -42,15 +41,78 @@ public class Matrix {
             throw new IllegalArgumentException("The provided values array doesn't match the matrix dimensions.");
         }
     }
-
     public void printMatrix() {
         for(int i = 0; i < this.rows; ++i) {
             for(int j = 0; j < this.columns; ++j) {
                 System.out.print(this.data[i][j] + " ");
             }
-
             System.out.println();
         }
-
+        System.out.println("\n");
     }
+    public int getNumRows() {
+        return rows;
+    }
+    public int getNumColumns() {
+        return columns;
+    }
+
+    // Getter для елементу
+    public double getElement(int row, int column) {
+        if (row < 0 || row >= rows || column < 0 || column >= columns) {
+            throw new IllegalArgumentException("Invalid row or column index");
+        }
+        return data[row][column];
+    }
+    // Getter для рядка
+    public double[] getRow(int rowIndex) {
+        if (rowIndex < 0 || rowIndex >= rows) {
+            throw new IllegalArgumentException("Invalid row index");
+        }
+        return Arrays.copyOf(data[rowIndex], columns);
+    }
+    // Getter для стовпця
+    public double[] getColumn(int columnIndex) {
+        if (columnIndex < 0 || columnIndex >= columns) {
+            throw new IllegalArgumentException("Invalid column index");
+        }
+        double[] columnData = new double[rows];
+        for (int i = 0; i < rows; i++) {
+            columnData[i] = data[i][columnIndex];
+        }
+        return columnData;
+    }
+    // Setter для елементу
+    public void setElement(int row, int column, double value) {
+        if (row < 0 || row >= rows || column < 0 || column >= columns) {
+            throw new IllegalArgumentException("Invalid row or column index");
+        }
+        data[row][column] = value;
+    }
+    // Setter для рядка
+    public void setRow(int rowIndex, double[] values) {
+        if (rowIndex < 0 || rowIndex >= rows || values.length != columns) {
+            throw new IllegalArgumentException("Invalid row index or values array length");
+        }
+        System.arraycopy(values, 0, data[rowIndex], 0, columns);
+    }
+    // Setter для стовпця
+    public void setColumn(int columnIndex, double[] values) {
+        if (columnIndex < 0 || columnIndex >= columns || values.length != rows) {
+            throw new IllegalArgumentException("Invalid column index or values array length");
+        }
+        for (int i = 0; i < rows; i++) {
+            data[i][columnIndex] = values[i];
+        }
+    }
+    public void RandomMatrix() {
+        Random random = new Random();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                data[i][j] = random.nextDouble(); // Можна використовувати інші методи Random для інших типів даних
+            }
+        }
+    }
+
+
 }
